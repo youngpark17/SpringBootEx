b1da7053954536e49e7227f51b788c7b7faad4b9
YUP <mduddns@naver.com>
2020-06-02 23:21:07 +0900

    readme

---
+++ lab5/src/main/java/com/example/demo/dao/BoardDAO.java
@@ -0,0 +1,11 @@
+package com.example.demo.dao;
+
+import com.example.demo.dto.BoardDTO;
+
+public interface BoardDAO {
+
+    int newBoard(BoardDTO param) throws Exception;
+    //6번째 줄: MyBatis의 <insert>는 INSERT에 성공했을 때 정수 1을 반환합니다.
+    BoardDTO getBoard(BoardDTO param) throws Exception;
+    int editBoard(BoardDTO param) throws Exception;
+}
